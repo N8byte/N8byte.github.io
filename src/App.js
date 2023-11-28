59abb827752b95bb7c35b9cfefb1f3b0eae7c602
Nathan Waskiewicz <nathanjwask@gmail.com>
2023-11-28 16:33:49 -0500

    Added nunito font

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ function App() {
   return (
     <>
       <h1>Nate's Website</h1>
-      <p style={{ opacity: 0.5 }}>
-        <i>Made in React</i>
+      <p>
+        <i className="subtitle">Made in React </i>
         <img src={logo} className="react-logo" alt="React logo" />
       </p>
-      <ChecklistItem checked={true} text="The thing of the to be checked" />
-      <ChecklistItem checked={false} text="Create personal website" />
-      <ChecklistItem checked={false} text="Create Rust project" />
+      <div className="checklist">
+        <ChecklistItem checked={true} text="The thing of the to be checked" />
+        <ChecklistItem checked={false} text="Create personal website" />
+        <ChecklistItem checked={false} text="Create Rust project" />
+      </div>
     </>
   );
 }
