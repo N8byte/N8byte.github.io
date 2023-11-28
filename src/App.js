1b78b6acfb08c0d54f895a511889f38ce60544fd
Nathan Waskiewicz <nathanjwask@gmail.com>
2023-11-27 19:11:56 -0500

    Website draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Nate's Website</h1>
+      <p style={{ opacity: 0.5 }}>Made in React⚛️</p>
+      <input type="checkbox" checked readOnly></input>
+      <label>The thing of the to be checked</label>
+      <br />
+      <input type="checkbox" readOnly></input>
+      <label>Create personal website</label>
+      <br />
+      <input type="checkbox" readOnly></input>
+      <label>Create Rust project</label>
+    </>
   );
 }
 
