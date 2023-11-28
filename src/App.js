ec4eb743e3bda12578912674edc176440cba8b0d
Nathan Waskiewicz <nathanjwask@gmail.com>
2023-11-27 20:55:35 -0500

    - dark background - checklist to components - spinning logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Nate's Website</h1>
-      <p style={{ opacity: 0.5 }}>Made in React⚛️</p>
-      <input type="checkbox" checked readOnly></input>
-      <label>The thing of the to be checked</label>
+      <p style={{ opacity: 0.5 }}>
+        <i>Made in React</i>
+        <img src={logo} className="react-logo" alt="React logo" />
+      </p>
+      <ChecklistItem checked={true} text="The thing of the to be checked" />
+      <ChecklistItem checked={false} text="Create personal website" />
+      <ChecklistItem checked={false} text="Create Rust project" />
+    </>
+  );
+}
+
+function ChecklistItem(props) {
+  return (
+    <>
+      <input
+        type="checkbox"
+        checked={props.checked}
+        readOnly
+        onClick={() => {}}
+      />
+      <label>{props.text}</label>
       <br />
-      <input type="checkbox" readOnly></input>
-      <label>Create personal website</label>
-      <br />
-      <input type="checkbox" readOnly></input>
-      <label>Create Rust project</label>
     </>
   );
 }
