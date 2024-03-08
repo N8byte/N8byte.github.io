a065157e9dcf3487b7e0edfb0b326c2639c19b58
Nathan Waskiewicz <nathanjwask@gmail.com>
2024-03-07 22:26:37 -0500

    - Added short bio - Added rudimentary projects section

--- src/App.js
+++ src/App.js
@@ -6,15 +6,46 @@ function App() {
   return (
     <>
       <Navbar />
-      <h1>Nathan Waskiewicz</h1>
-      <p>
-        <i className="subtitle">Made in React </i>
-        <img src={reactLogo} className="react-logo" alt="React logo" />
-      </p>
-      <div className="checklist">
-        <ChecklistItem checked text="The thing of the to be checked" />
-        <ChecklistItem text="Create personal website" />
-        <ChecklistItem text="Create AWS/GCP project" />
+      <div className="main-div">
+        <p>
+          <i className="subtitle">Made in React </i>
+          <img src={reactLogo} className="react-logo" alt="React logo" />
+        </p>
+        <h1>Nathan Waskiewicz</h1>
+        <div className="panel">
+          <p>
+            Hi! I'm Nate, I'm a software engineer & designer, and I love what I
+            do.
+            <br />I try to make things beautiful, and I'm always solving
+            problems.
+          </p>
+        </div>
+        <br />
+        <h2>TODO</h2>
+        <div className="checklist">
+          <ChecklistItem checked text="The thing of the to be checked" />
+          <ChecklistItem text="Create personal website" />
+          <ChecklistItem text="Create AWS/GCP project" />
+        </div>
+        <br />
+        <h2>Projects</h2>
+        <div className="projects">
+          <div className="project">
+            <div className="card">Project A</div>
+          </div>
+          <div className="project">
+            <div className="card">Project B</div>
+          </div>
+          <div className="project">
+            <div className="card">Project C</div>
+          </div>
+          <div className="project">
+            <div className="card">Project D</div>
+          </div>
+          <div className="project">
+            <div className="card">Project E</div>
+          </div>
+        </div>
       </div>
     </>
   );
