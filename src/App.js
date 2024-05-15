ebc145cfe455cd6788bf1a2886f9968ed44c9c10
Nathan Waskiewicz <nathanjwask@gmail.com>
2024-05-15 18:07:57 -0400

    projects component TODO: fix title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import reactLogo from "./assets/images/react.svg";
 import logo from "./assets/images/logo.svg";
 import gcp from "./assets/images/gcp.png";
+import bandit from "./assets/images/bandit-cropped.png";
 import "./App.css";
 
 function App() {
@@ -27,30 +28,23 @@ function App() {
           <ChecklistItem checked text="Personal website" />
           <ChecklistItem text="AWS/GCP project" />
           <ChecklistItem text="Finish developing a game" />
+          <ChecklistItem checked text="Beat Bandit CTF" />
         </div>
         <br />
         <h2>Projects</h2>
         <div className="projects">
-          <div className="project">
-            <div className="card">Project A</div>
-            <img className="card-image" src={gcp} alt="GCP logo"></img>
-          </div>
-          <div className="project">
-            <div className="card">Project B</div>
-            <img className="card-image" src={gcp} alt="GCP logo"></img>
-          </div>
-          <div className="project">
-            <div className="card">Project C</div>
-            <img className="card-image" src={gcp} alt="GCP logo"></img>
-          </div>
-          <div className="project">
-            <div className="card">Project D</div>
-            <img className="card-image" src={gcp} alt="GCP logo"></img>
-          </div>
-          <div className="project">
-            <div className="card">Project E</div>
-            <img className="card-image" src={gcp} alt="GCP logo"></img>
-          </div>
+          <Project
+            name="Bandit CTF"
+            image={bandit}
+            description="I remember starting this in high school. Now I finally finished
+            this challenge! A beginner friendly CTF style challenge teaching
+            you the basics of shell scripting."
+          />
+          <Project
+            name="GCP/AWS Project"
+            image={gcp}
+            description="Coming Soon!"
+          />
         </div>
       </div>
       <br />
@@ -83,6 +77,16 @@ function ChecklistItem(props) {
       <label> {props.text}</label>
       <br />
     </>
+  );
+}
+
+function Project(props) {
+  return (
+    <div className="project">
+      <div className="card-title">{props.name}</div>
+      <img className="card-image" src={props.image} alt="Project logo"></img>
+      <span className="card-description">{props.description}</span>
+    </div>
   );
 }
 
